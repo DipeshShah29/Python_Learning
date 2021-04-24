4a51ae1f3be5a8580df5d2ac2e608e548b42470b
dipeshshah29 <ariesdips@gmail.com>
2021-04-24 14:26:58 +0530

    OOPs Concepts in Python

---
+++ First_Project_Object_&_Class.py
@@ -0,0 +1,30 @@
+# To check whether an Employee has achieved a weekly sales target or not
+# Take input from users as Name and Sales target per week
+
+
+class Employee:
+    def __init__(self, name, target):
+        self.name = name
+        self.target = target
+
+    def hasTargetAchieved(self):
+        if self.target >= 6:
+            print("The weekly target has been achieved by", self.name)
+        else:
+            print("The weekly target has not been achieved by", self.name)
+
+
+employee1 = Employee("Dipesh", 6)
+employee1.hasTargetAchieved()
+
+# Using **kwargs (keyword arguments)
+class EmployeeArgs:
+    def hasTargetAchievedNew(self, **params):
+        if params["target"] >= 6:
+            print("The weekly target has been achieved by", params["name"])
+        else:
+            print("The weekly target has not been achieved by", params["name"])
+
+
+employee2 = EmployeeArgs()
+employee2.hasTargetAchievedNew(name="Dipesh", target=4)
